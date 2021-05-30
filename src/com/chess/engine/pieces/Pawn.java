e39e01f1f624c41ea516e2593da20863afbcda34
Tushar Sharma <tushar123cse@gmail.com>
2021-05-30 09:04:14 +0530

    Pawn functinality addition

--- src/com/chess/engine/pieces/Pawn.java
+++ src/com/chess/engine/pieces/Pawn.java
@@ -23,13 +23,22 @@ public class Pawn extends Piece{
     public Collection<Move> calculateLegalMoves(Board board) {
         final List<Move> legalMoves = new ArrayList<>();
         for(final int currentCandidateOffset : CANDIDATE_MOVE_COORDINATE){
-            int candidateDestinationCoordinate = this.piecePosition +
+            final int candidateDestinationCoordinate = this.piecePosition +
                     (this.getPieceAlliance().getDirection() * currentCandidateOffset);
             if(!BoardUtil.isValidTileCoordinate(candidateDestinationCoordinate)){
                 continue;
             }
             if(currentCandidateOffset == 8 && !board.getTile(candidateDestinationCoordinate).isTileOccupied()){
-                legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
+                //TODO more work to do here (deal with promotions)
+                legalMoves.add(new MajorMove(board,this, candidateDestinationCoordinate));
+            } else if(currentCandidateOffset == 16 && this.isFirstMove() &&
+                     (BoardUtil.SECOND_ROW[this.piecePosition] && this.getPieceAlliance().isBlack())||
+                     (BoardUtil.SEVENTH_ROW[this.piecePosition] && this.getPieceAlliance().isWhite())){
+                final int behindCandidateDestinationCoordinate = this.piecePosition + (this.pieceAlliance.getDirection() * 8);
+                if(!board.getTile(behindCandidateDestinationCoordinate).isTileOccupied() &&
+                        !board.getTile(candidateDestinationCoordinate).isTileOccupied()){
+                    legalMoves.add(new MajorMove(board,this,candidateDestinationCoordinate));
+                }
             }
         }
         return legalMoves;
